be83fd1c20fcb21817610e6d8de70fcb03a8b62e
Bahaa <Bahaa@DESKTOP-FA8A547>
2023-05-15 00:31:02 -0700

    Add project files.

---
+++ MultiUserBank/MultiUsers.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiUserBank
+{
+    internal class MultiUserBank
+    {
+        decimal balance;
+        public string userName;
+        public string passWord;
+        decimal initialbalance = 10000;
+
+        public string enteryUsername { get; set; }
+        public string enterypassword { get; set; }
+        public int no { get; set; }
+        public Decimal money { get; set; }
+        public MultiUserBank(string userName, string passWord, decimal balance)
+        {
+            this.balance = balance;
+            this.userName = userName;
+            this.passWord = passWord;
+
+        }
+        public MultiUserBank()
+        {
+
+        }
+        public decimal _balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+        public decimal _initialbalance
+        {
+            get { return initialbalance; }
+            set { initialbalance = value; }
+        }
+        public void EnterUser()
+        {
+            Console.Clear();
+            Console.WriteLine(" MultiUsersList ");
+            Console.WriteLine(" ======== ");
+            Console.WriteLine(" ENTER User Name: ");
+            enteryUsername = Console.ReadLine();
+            Console.WriteLine(" ENTER Password : ");
+            enterypassword = Console.ReadLine();
+        }
+        public void enterMenu()
+        {
+
+            Console.Clear();
+            Console.WriteLine("... Main Menu ....");
+            Console.WriteLine("===================");
+            Console.WriteLine("(1) withdraw");
+            Console.WriteLine("(2) deposite");
+            Console.WriteLine("(3) check your balance");
+            Console.WriteLine("(0) for EXIT");
+            Console.WriteLine("===================");
+
+        }
+        public void withdraw()
+        {
+            Console.Clear();
+            Console.WriteLine("your current Balance is : " + string.Format("${0:0.00$;'neg: $'-0.00;zero}", _balance));
+            Console.WriteLine("Enter withdraw money  : ");
+            money = decimal.Parse(Console.ReadLine());
+            if (money > 500) { money = 500; }
+            if (money > _balance) { money = _balance; }
+            Console.WriteLine("The amount withdrawn is  : " + string.Format("${0:0.00$;'neg: $'-0.00;zero}", money));
+
+            _balance -= money;
+            _initialbalance -= money;
+        }
+
+        public void deposite()
+        {
+            Console.Clear();
+            Console.WriteLine("your current Balance  : " + string.Format("${0:0;'neg: $'-0;zero}", _balance));
+            Console.WriteLine("Enter deposite money  : ");
+            money = decimal.Parse(Console.ReadLine());
+
+            _balance += money;
+            _initialbalance += money;
+        }
+        public void check_balance()
+        {
+            Console.Clear();
+            Console.WriteLine("your current Balance is : " + string.Format("${0:0;'neg: $'-0;zero}", _balance));
+        }
+
+
+    }
+}
+
---
+++ MultiUserBank/Program.cs
@@ -0,0 +1,94 @@
+﻿
+       namespace MultiUserBank
+    {
+        // Elbehiri
+        // IT112
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                // variable to Search for users & passwords in Array
+                Boolean found;
+                //postion customer for array
+                int j = 0;
+                //create  object using Array for 4 customes 
+                MultiUserBank[] customer = new MultiUserBank[4];
+
+                MultiUserBank b = new MultiUserBank();
+
+                //store customers data in Array   
+                customer[0] = new MultiUserBank("jlennon", "johnny", 1250m);
+                customer[1] = new MultiUserBank("pmccartney", "pauly", 2500m);
+                customer[2] = new MultiUserBank("gharrison", "georgy", 3000m);
+                customer[3] = new MultiUserBank("rstarr", "ringoy", 1000m);
+
+                Console.Clear();
+
+            ineligible:
+                //Enter and verify user data and password
+                b.EnterUser();
+
+                //   
+                found = false;
+                for (int i = 0; i <= 3; i++)
+                {
+
+                    if (b.enteryUsername == customer[i].userName && b.enterypassword == customer[i].passWord)
+                    {
+                        b._balance = customer[i]._balance;
+                        j = i;
+                        found = true;
+                        break;
+                    }
+
+                }
+
+                if (found == false)
+                {
+                    Console.WriteLine("error username or password");
+                    Console.ReadLine();
+                    b.EnterUser();
+                    //Re-enter and verify the user data and password
+                    goto ineligible;
+                }
+
+                else
+                {
+                    do
+                    {
+                        // View the bank's list of withdrawals and deposits
+                        b.enterMenu();
+
+                        b.no = int.Parse(Console.ReadLine());
+                        switch (b.no)
+                        {
+                            case 1:
+                                // methods withdraw
+                                b.withdraw();
+                                customer[j]._balance = b._balance;
+                                Console.WriteLine("your Balance After withdraw is : " + string.Format("${0:0;'neg: '-0$;zero}", customer[j]._balance));
+
+                                Console.ReadLine();
+                                break;
+                            case 2:
+                                //  methods deposite
+                                b.deposite();
+                                customer[j]._balance = b._balance;
+                                Console.WriteLine("your Balance After deposite is : " + string.Format("${0:0;'neg: '-0$;zero}", customer[j]._balance));
+
+                                Console.ReadLine();
+                                break;
+                            case 3:
+                                // methods check balance
+                                b.check_balance();
+                                Console.ReadLine();
+                                break;
+
+                        }
+                    } while (b.no != 0 || b.no > 3);
+                    //  Console.ReadLine();
+                }
+            }
+        }
+    }
